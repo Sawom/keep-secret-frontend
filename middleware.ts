fb1ac2a677d77e91b2c14dcd380da53886258c07
Sawom <asawom250@gmail.com>
2026-09-13 01:19:45 +0600

    middleware fixed

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    return NextResponse.next();
+    const response = NextResponse.next();
+
+    // ব্রাউজার যেন পেজ ব্যাক করলে ক্যাশ থেকে না দেখিয়ে নতুন করে ভ্যালিডেট করে
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+
+    return response;
 }
 
 export const config = {
